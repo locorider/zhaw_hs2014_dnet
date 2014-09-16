b2a886b2de327b579276b1f8e77dfd05072ad541
José Rota <jose.rota@bluewin.ch>
2014-09-16 07:44:21 +0200

    initial commit

---
+++ P01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                string sizeStr = args[0];
+                int size = Int32.Parse(sizeStr);
+                Sieve sieve = new Sieve();
+                PrimeType[] types = sieve.Eratosthenes(size);
+                PrintResult(types);
+            }
+            else
+            {
+                Console.Error.WriteLine("Please provide the size of the sieve");
+            }
+        }
+
+        private static void PrintResult(PrimeType[] types)
+        {
+            for (int i = 0, l = types.Length; i < l; i++)
+            {
+                PrimeType type = types[i];
+                int num = i + 1;
+                if (0 < i && i % 10 == 0) {
+                    Console.Write("\r\n");
+                }
+                if (PrimeType.NotPrim == type)
+                {
+                    Console.Write("_ ");
+                }
+                else
+                {
+                    Console.Write(num.ToString() + " ");
+                }
+            }
+        }
+    }
+}
---
+++ P01/Sieve.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01
+{
+    class Sieve
+    {
+        public PrimeType[] Eratosthenes(int size)
+        {
+            PrimeType[] result = new PrimeType[size];
+            for (int i = 1; i < size; i++)
+            {
+                int number = i + 1;
+                if (number * number <= size)
+                {
+                    for (int j = i + 1; j < size; j++)
+                    {
+                        int nextNumber = j + 1;
+                        if (nextNumber % number == 0)
+                        {
+                            result[j] = PrimeType.NotPrim;
+                        }
+                    }
+                }
+                
+            }
+            return result;
+        }
+
+        public List<int> EratosthenesList(int size)
+        {
+            List<int> result = new List<int>();
+            PrimeType[] types = Eratosthenes(size);
+            for (int i = 0, l = types.Length; i < l; i++)
+            {
+                PrimeType type = types[i];
+                if (PrimeType.Prim == type)
+                {
+                    result.Add(i + 1);
+                }
+            }
+            return result;
+        }
+
+        public int[] EratosthenesArray(int size)
+        {
+            List<int> list = EratosthenesList(size);
+            int[] result = list.ToArray();
+            return result;
+        }
+
+        public Dictionary<int, int> EratosthenesDict(int size)
+        {
+            Dictionary<int, int> result = new Dictionary<int, int>();
+            List<int> list = EratosthenesList(size);
+            int idx = 0;
+            foreach (int number in list)
+            {
+                result.Add(idx++, number);
+            }
+            return result;
+        }
+    }
+}
